2d95ad0f3edcbc62f14f7a71a8e18bb715eea1fb
Ruby Shrestha <ruby.shrestha1@louisiana.edu>
2021-11-09 03:11:00 +0000

    Updated admin.py by removing Characters_1 table as it was removed from models.py

--- BrawlNotes/admin.py
+++ BrawlNotes/admin.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Placements_1, Characters_1, Player_Registry, Official_1_Events
+from .models import Placements_1, Player_Registry, Official_1_Events
 
 admin.site.register(Placements_1)
-admin.site.register(Characters_1)
 admin.site.register(Player_Registry)
 admin.site.register(Official_1_Events)
